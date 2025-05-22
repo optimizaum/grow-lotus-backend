d7f7e47688a962d07eb817875ac527896b89288f
Rajat Appmingle <rajat.gupta@appminglemedia.com>
2025-05-22 18:54:22 +0530

    changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/uploads", express.static("./src/upload"));
+app.use("/api/v1/uploads", express.static("./src/upload"));
 app.use(cookieParser());
 // Error-handling middleware (should come last)
 app.use(errorHandler);
